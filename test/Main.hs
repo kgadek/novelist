2c20b97e181c834c65f68aae8807147680591d71
Konrad Gądek <kgadek@gmail.com>
2017-02-07 15:12:11 +0100

    tests: output XML file, place in proper path

--- test/Main.hs
+++ test/Main.hs
@@ -1,10 +1,36 @@
 module Main where
 
+-- base
+import           System.IO (withFile, IOMode(WriteMode))
+
+-- unix
+import           System.Posix.Env (getEnvDefault)
+
+-- filepath
+import           System.FilePath.Posix ((</>))
+
+-- directory
+import           System.Directory (createDirectoryIfMissing)
+
 -- hspec
 import           Test.Hspec.Runner
-import           Test.Hspec.Formatters
+
+-- hspec-jenkins
+import           Test.Hspec.Formatters.Jenkins (xmlFormatter)
+
+--
 import qualified Spec
 
+
 main :: IO ()
-main = hspecWith config Spec.spec
-  where config = defaultConfig
+main = do
+    resultDirRoot <- getEnvDefault "CIRCLE_TEST_REPORTS" "."
+    let resultDir = resultDirRoot </> "junit"
+        resultPath = resultDir </> "results.xml"
+    createDirectoryIfMissing True resultDir
+
+    withFile resultPath WriteMode $ \h -> do
+        let config = defaultConfig { configFormatter = Just xmlFormatter
+                                   , configOutputFile = Left h
+                                   }
+        hspecWith config Spec.spec
